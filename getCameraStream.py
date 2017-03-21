dd918910bb5a5eb95a373778ccd2187cf0e7dca3
precis308acs <precis308acs@gmail.com>
2017-03-21 19:00:01 +0200

    Finished Camera Provider

--- getCameraStream.py
+++ getCameraStream.py
@@ -12,70 +12,70 @@ import numpy
 import cv2
 
 
+def connect_video_device(IP, PORT, FPS):
+	camProxy = ALProxy("ALVideoDevice", IP, PORT)
+	resolution = 2    # VGA
+	colorSpace = 11   # RGB
+	videoClient = camProxy.subscribe("python_client", resolution, colorSpace, FPS)
+
 def getImage(camProxy):
   
-  # Get a camera image.
-  # image[6] contains the image data passed as an array of ASCII chars.
-  t0 = time.time()
-  naoImage = camProxy.getImageRemote(videoClient)
+	# Get a camera image.
+	# image[6] contains the image data passed as an array of ASCII chars.
+	t0 = time.time()
+	naoImage = camProxy.getImageRemote(videoClient)
 
-  t1 = time.time()
+	t1 = time.time()
 
-  # Time the image transfer.
-  print "acquisition delay ", t1 - t0
+	# Time the image transfer.
+	print "acquisition delay ", t1 - t0
 
 
 
-  # Now we work with the image returned and save it as a PNG  using ImageDraw
-  # package.
+	# Now we work with the image returned and save it as a PNG  using ImageDraw
+	# package.
 
-  # Get the image size and pixel array.
-  imageWidth = naoImage[0]
-  imageHeight = naoImage[1]
-  array = naoImage[6]
+	# Get the image size and pixel array.
+	imageWidth = naoImage[0]
+	imageHeight = naoImage[1]
+	array = naoImage[6]
 
-  # Create a PIL Image from our pixel array.
-  im = Image.fromstring("RGB", (imageWidth, imageHeight), array)
-  opencvImage = cv2.cvtColor(numpy.array(im), cv2.COLOR_RGB2BGR)
-  return opencvImage
+	# Create a PIL Image from our pixel array.
+	im = Image.fromstring("RGB", (imageWidth, imageHeight), array)
+	opencvImage = cv2.cvtColor(numpy.array(im), cv2.COLOR_RGB2BGR)
+	return opencvImage
  
 
 if __name__ == '__main__':
-  IP = "nao.local"  # Replace here with your NaoQi's IP address.
-  PORT = 9559
-  FPS = 20
+	IP = "nao.local"  # Replace here with your NaoQi's IP address.
+	PORT = 9559
+	FPS = 20
 
-  # Read IP address from first argument if any.
-  if len(sys.argv) > 1:
-    IP = sys.argv[1]
-    if(len(sys.argv) == 3):
-    	if sys.argv[2].isdigit():
-    		FPS = int(sys.argv[2])
-    	else:
-    		print("FPS is not numerical!")
+	# Read IP address from first argument if any.
+	if len(sys.argv) > 1:
+		IP = sys.argv[1]
+	if(len(sys.argv) == 3):
+		if sys.argv[2].isdigit():
+			FPS = int(sys.argv[2])
+		else:
+			print("FPS is not numerical!")
 
-    	print(FPS)
-  else:
-  	print("Usage: getCameraStream <Pepper_IP> OPT:<FPS>")
-  	exit(-1)
+		print(FPS)
+	else:
+		print("Usage: getCameraStream <Pepper_IP> OPT:<FPS>")
+		exit(-1)
 
-  	"""
-  First get an image from Nao, then show it on the screen with PIL.
-  """
+		"""
+	First get an image from Nao, then show it on the screen with PIL.
+	"""
 
-  camProxy = ALProxy("ALVideoDevice", IP, PORT)
-  resolution = 2    # VGA
-  colorSpace = 11   # RGB
-  print("Desired FPS:" + str(FPS))
-  videoClient = camProxy.subscribe("python_client", resolution, colorSpace, FPS)
-
-  
-  while(True):
-  	img = getImage(camProxy)
-  	cv2.imshow("Image", img)
-  	k = cv2.waitKey(1)
+	camProxy = connect_video_device(IP, PORT, FPS)
+	while(True):
+		img = getImage(camProxy)
+		cv2.imshow("Image", img)
+		k = cv2.waitKey(1)
 	if k==27:    # Esc key to stop
 		cv2.destroyAllWindows()
 		break
 
-  camProxy.unsubscribe(videoClient)
+	camProxy.unsubscribe(videoClient)
